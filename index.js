d40c763cc6f472aa583a8c144054a32266a76327
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2021-10-07 15:08:35 +0000

    ✨ Support limit

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+/**
+ * @typedef {'postgresql'|'mysql'|'sqlserver'} Dialect
+ *
+ * @typedef {{[key: number]: string}} Fields
+ *
+ *
+ * @typedef Query
+ * @property {number|null} limit
+ * @property {Where|null} where
+ *
+ * @typedef {[Operator, ...any[]]} Where
+ *
+ * @typedef {'and'|'or'|'not'|'<'|'>'|'='|'!='|'is-empty'|'not-empty'} Operator
+ */
+
+/**
+ *
+ * @param {Dialect} dialect
+ * @param {Fields} fields
+ * @param {Query} query
+ * @returns any
+ */
 module.exports = function generateSql(dialect, fields, query) {
-  return 'test'
+  const { limit } = query
+  if (dialect === 'sqlserver') {
+    const limitClause = limit ? `TOP ${limit} ` : ''
+    return `SELECT * ${limitClause}FROM data;`
+  }
+
+  // `mysql` and `postgresql`
+  const limitClause = limit ? ` LIMIT ${limit}` : ''
+  return `SELECT * FROM data${limitClause};`
 }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,60 @@
 const generateSql = require('./index')
 
-test('test jest', () => {
-  expect(generateSql()).toEqual('test')
+test('test postgresql with no query', () => {
+  const dialect = 'postgresql'
+  const query = { limit: null, where: null }
+  const fields = { 1: 'name', 2: 'location' }
+
+  expect(generateSql(dialect, fields, query)).toEqual(`SELECT * FROM data;`)
+})
+
+test('test mysql with no query', () => {
+  const dialect = 'mysql'
+  const query = { limit: null, where: null }
+  const fields = { 1: 'name', 2: 'location' }
+
+  expect(generateSql(dialect, fields, query)).toEqual(`SELECT * FROM data;`)
+})
+
+test('test sqlserver with no query', () => {
+  const dialect = 'sqlserver'
+  const query = { limit: null, where: null }
+  const fields = { 1: 'name', 2: 'location' }
+
+  expect(generateSql(dialect, fields, query)).toEqual(`SELECT * FROM data;`)
+})
+
+test('test postgresql limit', () => {
+  const dialect = 'postgresql'
+  const query = { limit: 10, where: null }
+  const fields = { 1: 'name', 2: 'location' }
+
+  expect(generateSql(dialect, fields, query)).toEqual(`SELECT * FROM data LIMIT 10;`)
+})
+
+test('test mysql limit', () => {
+  const dialect = 'mysql'
+  const query = { limit: 10, where: null }
+  const fields = { 1: 'name', 2: 'location' }
+
+  expect(generateSql(dialect, fields, query)).toEqual(`SELECT * FROM data LIMIT 10;`)
+})
+
+test('test sqlserver limit', () => {
+  const dialect = 'sqlserver'
+  const query = { limit: 10, where: null }
+  const fields = { 1: 'name', 2: 'location' }
+
+  expect(generateSql(dialect, fields, query)).toEqual(`SELECT * TOP 10 FROM data;`)
+})
+
+test.skip('test query', () => {
+  const dialect = 'postgresql'
+  /**
+   * @type {import('./index').Query}
+   */
+  const query = { limit: null, where: ['=', ['field', 1], 'cam'] }
+  const fields = { 1: 'name', 2: 'location' }
+
+  expect(generateSql(dialect, fields, query)).toEqual(`SELECT * FROM data WHERE name = 'cam';`)
 })
